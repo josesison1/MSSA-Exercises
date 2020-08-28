51fa4f5651fbea73e8e7e65a84ab8d267387f1a7
Jose Sison <68295496+josesison1@users.noreply.github.com>
2020-08-27 17:54:07 -0700

    Add files via upload

---
+++ IntegerSwap/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace IntegerSwap
+{
+    class Program
+    {
+        static void IntSwap()
+        {
+            int x = 4;
+            int y = 10;
+
+            Console.WriteLine("\n\nBefore swap: x=" + x + " y=" + y);
+            x = x + y;
+            y = x - y;
+            x = x - y;
+            Console.WriteLine("After swap: x=" + x + " y=" + y);
+        }
+        static void Main(string[] args)
+        {
+            IntSwap();
+
+        }
+    }
+}
+
+
+    
+

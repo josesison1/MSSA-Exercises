fd77d6b79fd74a37f6341a4623977d2f913e4c09
Jose Sison <68295496+josesison1@users.noreply.github.com>
2020-08-28 21:22:49 -0700

    Add files via upload

--- CodingPractice/CodingPractice/Program.cs
+++ CodingPractice/CodingPractice/Program.cs
@@ -6,55 +6,60 @@ namespace CodingPractice
     {
         static void Main(string[] args)
         {
+
+
             /******************* ITERATE THROUGH 10 AND REVERSE ************************/
 
-            for (int i = 1; i <= 10; i++)
-            {
-                if (i % 2 == 0)
-                    Console.WriteLine(i);
-            }
-            for (int i = 10; i > 1; i--)
-            {
-                if (i % 2 == 0)
-                    Console.WriteLine(i);
-            }
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    if (i % 2 == 0)
+            //        Console.WriteLine(i);
+            //}
+            //for (int i = 10; i > 1; i--)
+            //{
+            //    if (i % 2 == 0)
+            //        Console.WriteLine(i);
+            //}
 
 
             /********* ITERATE THROUGH AND PRINT EACH CHAR IN STRING USING FOR LOOP AND FOREACH LOOP ************/
 
-            string name = "Jose Sison";
+            //string name = "Jose Sison";
 
-            for (int i = 0; i <= name.Length; i++)
-            {
-                Console.WriteLine(name[i]);
-            }
+            //for (int i = 0; i <= name.Length; i++)
+            //{
+            //    Console.WriteLine(name[i]);
+            //}
 
-            foreach (var names in name)
-            {
-                Console.WriteLine(names);
-            }
+            //foreach (var names in name)
+            //{
+            //    Console.WriteLine(names);
+            //}
 
             /************* ITERATE THROUGH NUMBER USING WHILE LOOP *************************/
 
-            int i = 0;
-            while (i <= 10)
-            {
-                if (i % 2 == 0)
-                    Console.WriteLine(i);
-                i++;
-            }
+            //int i = 0;
+            //while (i <= 10)
+            //{
+            //    if (i % 2 == 0)
+            //        Console.WriteLine(i);
+            //    i++;
+            //}
 
-            while (true)
-            {
-                Console.Write("What is your name? ");
-                string input = Console.ReadLine();
+            //while (true)
+            //{
+            //    Console.Write("What is your name? ");
+            //    string input = Console.ReadLine();
 
-                if (string.IsNullOrEmpty(input))
-                {
-                    Console.WriteLine("Please enter a valid format.");
-                    break;
-                }
-            }
+            //    if (string.IsNullOrEmpty(input))
+            //    {
+            //        Console.WriteLine("Please enter a valid format.");
+            //        continue;
+            //    }
+              //    break;
+            //}
+
+
         }
     }
 }

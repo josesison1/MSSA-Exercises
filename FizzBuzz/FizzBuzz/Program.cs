153742425dc82c3631f6d7dd032074c5b6843d0f
Jose Sison <68295496+josesison1@users.noreply.github.com>
2020-08-31 21:25:18 -0700

    Add files via upload

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n\t\t\t\t|********************************************|" +
+                                "\n\t\t\t\t|    F    I    Z    Z    B    U    Z    Z    |" +
+                                "\n\t\t\t\t|********************************************|");
+
+                for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}

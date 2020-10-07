b1be6e38c7580ef57faf0c94bb2676e66fac7009
Jose Sison <68295496+josesison1@users.noreply.github.com>
2020-10-06 18:44:10 -0700

    Add files via upload

---
+++ EvenSum/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Xml.Schema;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int EvenSum(int min, int max)
+        {
+            int result = 0;
+
+            for (int i = min; i <= max; i++)
+            {
+                if (i % 2 == 0) result += i;
+            }
+            return result;
+        }
+
+
+        static void Main(string[] args)
+        {
+            int total = EvenSum(2, 5);
+            Console.WriteLine(total);
+        }
+    }
+}

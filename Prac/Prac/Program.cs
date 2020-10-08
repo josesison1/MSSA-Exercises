81a32a885f12bfe6ee9c0f01a4f9f4a939f1b5fc
Jose Sison <68295496+josesison1@users.noreply.github.com>
2020-10-08 09:57:25 -0700

    Add files via upload

---
+++ Prac/Prac/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Xml.Schema;
+using System.Xml.XPath;
+
+namespace Prac
+{
+    class Program
+    {
+        // Given input of 2 numbers, if first number is divisible by the 2nd number return true;
+        static bool dividesEvenly(int a, int b)
+        {
+            if (a % b == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+
+            }
+        }
+
+        // Return if input is empty or not 
+
+        static bool isEmpty(string inputString)
+        {
+            if (inputString == "")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // Convert minutes into seconds  
+        static void secondsinmins()
+        {
+            Console.Write("Enter the minutes you want to convert into seconds: ");
+            int mins = Convert.ToInt32(Console.ReadLine());
+            int result = mins * 60;
+
+            Console.WriteLine($"there are {result} seconds in {mins} minutes");
+        }
+        static void Main(string[] args)
+        {
+            //SecondsInMins();
+            //isEmpty();
+            bool num = dividesEvenly(900, 90);
+            Console.WriteLine($"{ num}");
+            
+        }
+    }
+}

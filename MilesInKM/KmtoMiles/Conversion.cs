58d160fe7d61d24c4499c6aec0a2d0ca194f1da2
Jose Sison <68295496+josesison1@users.noreply.github.com>
2020-10-07 22:09:12 -0700

    Add files via upload

---
+++ MilesInKM/KmtoMiles/Conversion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KmtoMiles
+{
+    class Conversion
+    {
+        const double conversion = 1.609;
+        public static double KmToMiles(double kilo)
+        {
+            return kilo / conversion;                  
+        }
+
+        public static double MilesToKilo(double miles)
+        {
+            return miles * conversion;
+        }
+    }
+}
---
+++ MilesInKM/KmtoMiles/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace KmtoMiles
+{
+    class Program
+    {
+        static double KiloInMile  = 1.609;
+        static double MiToKm(double miles) => miles * KiloInMile;       
+       
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MiToKm(5));          
+       }
+    }
+}
